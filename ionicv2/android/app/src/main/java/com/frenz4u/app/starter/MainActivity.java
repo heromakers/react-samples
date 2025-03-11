5bdefeee50502575e8e2e6afebbc94fa910350e6
ytlim <jesusloveyt@gmail.com>
2025-03-12 08:14:23 +0900

    Ionic Capacitor in React typescript

---
+++ ionicv2/android/app/src/main/java/com/frenz4u/app/starter/MainActivity.java
@@ -0,0 +1,5 @@
+package com.frenz4u.app.starter;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ ionicv2/capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.frenz4u.app.starter',
+  appName: 'IonicCapacitor',
+  webDir: 'build'
+};
+
+export default config;
